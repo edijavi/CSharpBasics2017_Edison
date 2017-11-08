4ce6094f92bca9c2f29c235aaf88e8f9ca849efd
edijavi <edilata@msn.com>
2017-11-08 23:29:11 +0100

    VideoMenu

--- FirstCoreSolution/VideoMenu/Program.cs
+++ FirstCoreSolution/VideoMenu/Program.cs
@@ -1,79 +1,172 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VideoMenu
 {
     class Program
     {
+        static int id = 1;
+        static List<Video> videos = new List<Video>();
+
         static void Main(string[] args)
         {
-            
-            string[] menuItems =
+            var cust1 = new Video
             {
-                "Search Video",
+                Id= id++,
+                VideoName = "Bob",
+                VideoType = "Dilan",
+                VideoLocation = "BongoStreet 232"
+            };
+            videos.Add(cust1);
+
+            videos.Add(new Video()
+            {
+                Id=id++,
+                VideoName = "Edison",
+                VideoType = "Lamar",
+                VideoLocation = "Sp Kirkevej 129"
+            });
+
+            //Show the first video saved internally
+            //Console.WriteLine($"Name:{cust1.VideoName} {cust1.VideoType}");
+            string[] menuItems =
+            {           
+                "List All Videos",
                 "Add Video",
                 "Delete Video",
-                "Update Videos",
+                "Edit Video",
                 "Exit"
             };
 
+            //Show Menu
+            //Wait for Selection
+            // - Show selection or
+            // - Warning and go back to menu
 
-
-            var selection = ShowMenu(menuItems);
-
-            while (selection != 5)
+            var selection=ShowMenu(menuItems);
+            
+            while (selection!=5)
             {
-                
                 switch (selection)
                 {
                     case 1:
-                        Console.WriteLine("Search Video");
+                        ListVideos();                        
                         break;
                     case 2:
-                        Console.WriteLine("Add Video");
+                        AddVideo();
                         break;
                     case 3:
-                        Console.WriteLine("Delete Video");
+                        DeleteVideo();
                         break;
                     case 4:
-                        Console.WriteLine("Edit Video");
+                        EditVideo();
                         break;
 
                     default:
 
                         break;
                 }
-                
+
                 selection = ShowMenu(menuItems);
-                
+
             }
-            
-            Console.WriteLine("Back soon");
+
+            Console.WriteLine("Bye Bye!");
             Console.ReadLine();
 
+        }
 
+        private static void EditVideo()
+        {
+            var customer = FindVideoById();
+            Console.WriteLine("First Name: ");
+            customer.VideoName = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            customer.VideoType = Console.ReadLine();
+            Console.WriteLine("VideoLocation: ");
+            customer.VideoLocation = Console.ReadLine();
+        }
+
+        private static Video FindVideoById()
+        {
+            Console.WriteLine("Insert Video Id: ");
+            int id;
+            while (!int.TryParse(Console.ReadLine(), out id))
+            {
+                Console.WriteLine("Please insert a number");
+
+            }
+            foreach (var video in videos)
+            {
+                if (video.Id == id)
+                {
+                    return video;
+                }
+            }
+            return null;
+        }
+
+        private static void DeleteVideo()
+        {
+
+            var videoFound = FindVideoById();
+                        
+            if (videoFound != null)
+            {
+                videos.Remove(videoFound);
+            }
+        }
+
+        private static void AddVideo()
+        {
+            Console.WriteLine("Video Name: ");
+            var videoName = Console.ReadLine();
+            Console.WriteLine("Video Type: ");
+            var videoType = Console.ReadLine();
+            Console.WriteLine("VideoLocation: ");
+            var location = Console.ReadLine();
+
+            videos.Add(new Video()
+            {
+                Id = id++,
+                VideoName = videoName,
+                VideoType = videoType,
+                VideoLocation = location,
+            });
+
+        }
+
+        private static void ListVideos()
+        {
+            Console.WriteLine("\nList of Videos");
+          
+            foreach (var video in videos)
+            {
+                Console.WriteLine($"Id: {video.Id} Name: {video.VideoName} {video.VideoType} VideoLocation: {video.VideoLocation}");
+            }
+            Console.WriteLine("\n");
         }
 
         private static int ShowMenu(string[] menuItems)
         {
+            Console.WriteLine("Select What you want to do:\n ");
             
-            Console.WriteLine("Welcome Club Edi:\n ");
-
             for (int i = 0; i < menuItems.Length; i++)
             {
-                Console.WriteLine((i + 1) + ":" + menuItems[i]);
-
+                //Console.WriteLine((i + 1) + ":" + menuItems[i]);
+                Console.WriteLine($"{(i + 1)}: {menuItems[i]}");
             }
+            
 
             int selection;
             
-            while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > 5)
+            while (!int.TryParse(Console.ReadLine(), out selection) || selection<1 ||selection>5)
             {
-                Console.WriteLine("Please enter a valid option");
+                Console.WriteLine("You need to select number between 1-5");
             }
-            Console.WriteLine("Selection: " + selection+ "\n");
-            return selection;
-            
-        }
 
+            Console.WriteLine("Selection: " + selection);
+            return selection;
+        }
     }
 }
---
+++ FirstCoreSolution/VideoMenu/Video.cs
@@ -0,0 +1,16 @@
+﻿namespace VideoMenu
+{
+    //Blueprint
+    //public, internal, protected, private
+    class Video
+    {
+        public int Id { get; set; }
+
+        public string VideoName { get; set; }
+
+        public string VideoType { get; set; }
+
+        public string VideoLocation { get; set; }
+        
+    }
+}
